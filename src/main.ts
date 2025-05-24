696715a7ed28c06724a31aeaaea60632a2c96224
José Artur Coelho <procopioartur@gmail.com>
2025-05-24 11:15:50 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Fincheck API')
     .setDescription('A sua API para gerenciamento financeiro!')
     .setVersion('1.0')
-    .addTag('finances')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
