75e5f49199e74fd4052800461d10f5bb1a2588f7
José Artur Coelho <procopioartur@gmail.com>
2025-05-24 10:07:11 -0300

    feat: add OpenAPI swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Fincheck API')
+    .setDescription('A sua API para gerenciamento financeiro!')
+    .setVersion('1.0')
+    .addTag('finances')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 
